6b5a5dc99b4c211d973e8eabb73d6957f55d0953
genchi.lu[盧俊錡] <genchi.lu@genchilu-mac13.local>
2020-02-11 23:39:56 +0800

    Test

---
+++ src/main/java/test/MyTest2.java
@@ -0,0 +1,21 @@
+package test;
+
+public class MyTest2 {
+
+    public String test21(){
+        return "test1";
+    }
+
+    public String test22(){
+        return "test1";
+    }
+
+    public String test23(){
+        return "test1";
+    }
+
+    public String test24(){
+        return "test1";
+    }
+
+}
