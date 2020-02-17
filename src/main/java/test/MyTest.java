363512288d575faf2df701612f3769914826358a
genchi.lu[盧俊錡] <genchi.lu@genchilu-mac13.local>
2020-02-18 00:29:06 +0800

    Coverage fail

--- src/main/java/test/MyTest.java
+++ src/main/java/test/MyTest.java
@@ -21,4 +21,16 @@ public class MyTest {
     public String oldCodeFun5(){
         return "test1";
     }
+
+    public void newCodeFun1() throws InterruptedException {
+        Thread.sleep(1);
+    }
+
+    public void newCodeFun2() throws InterruptedException {
+        Thread.sleep(1);
+    }
+
+    public void newCodeFun3() throws InterruptedException {
+        Thread.sleep(1);
+    }
 }
