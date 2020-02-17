7e9934f32f0013aa881fb8582fbbb3f9c93e6e2c
genchi.lu[盧俊錡] <genchi.lu@genchilu-mac13.local>
2020-02-18 00:57:39 +0800

    1111

--- src/main/java/test/MyTest.java
+++ src/main/java/test/MyTest.java
@@ -23,14 +23,26 @@ public class MyTest {
     }
 
     public void newCodeFun1() throws InterruptedException {
+        System.out.println("廢 code");
+        System.out.println("廢 code");
+        System.out.println("廢 code");
+        System.out.println("廢 code");
         Thread.sleep(1);
     }
 
     public void newCodeFun2() throws InterruptedException {
+        System.out.println("廢 code");
+        System.out.println("廢 code");
+        System.out.println("廢 code");
+        System.out.println("廢 code");
         Thread.sleep(1);
     }
 
     public void newCodeFun3() throws InterruptedException {
+        System.out.println("廢 code");
+        System.out.println("廢 code");
+        System.out.println("廢 code");
+        System.out.println("廢 code");
         Thread.sleep(1);
     }
 }
